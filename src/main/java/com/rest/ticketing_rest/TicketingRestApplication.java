b3d0195d49daf825b48ac26d9d53b1d64643a778
ufuk058 <ufuksahinduran.uk@gmail.com>
2024-12-17 22:37:22 +0000

    initial commit

---
+++ src/main/java/com/rest/ticketing_rest/TicketingRestApplication.java
@@ -0,0 +1,13 @@
+package com.rest.ticketing_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketingRestApplication.class, args);
+	}
+
+}
