fa979a9f7dc36ac65700888b2875ac93d113ce53
ufuk058 <ufuksahinduran.uk@gmail.com>
2024-12-26 23:01:54 +0000

    Swagger authorization performed with the clientId and Client Secret added to properties file in order to get easy authorization on the Swagger file.

--- src/main/java/com/rest/ticketing_rest/config/SwaggerConfig.java
+++ src/main/java/com/rest/ticketing_rest/config/SwaggerConfig.java
@@ -1,8 +1,13 @@
 package com.rest.ticketing_rest.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.OAuthFlow;
+import io.swagger.v3.oas.annotations.security.OAuthFlows;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -16,7 +21,16 @@ import org.springframework.context.annotation.Configuration;
                         email = "support@moonstar.com",
                         url= "https://www.moonstar.com"
                 )
-        )
+        ),
+        security = {@SecurityRequirement(name="Keycloak")}
+)
+@SecurityScheme(
+      name = "Keycloak",
+      type= SecuritySchemeType.OAUTH2,
+      flows = @OAuthFlows(authorizationCode = @OAuthFlow(
+              authorizationUrl = "http://localhost:8080/realms/moonstar-dev/protocol/openid-connect/auth",
+              tokenUrl = "http://localhost:8080/realms/moonstar-dev/protocol/openid-connect/token"
+      ))
 )
 public class SwaggerConfig {
 
